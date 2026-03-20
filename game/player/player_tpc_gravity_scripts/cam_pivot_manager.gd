eb0cd7bd6938b3733bb199f7b7babd35fda97b55
adamvirtualspace-lab <adam.virtualspace@gmail.com>
2026-03-20 17:37:52 +0700

    basic player & camera script

---
+++ game/player/player_tpc_gravity_scripts/cam_pivot_manager.gd
@@ -0,0 +1,22 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+# Capturing/decapturing Mouse
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		if Input.get_mouse_mode() ==Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+# Mouse Movement
---
+++ game/player/player_tpc_gravity_scripts/player_tpc_gravity.gd
@@ -0,0 +1,49 @@
+extends RigidBody3D
+
+# Movement settings
+var movement_speed: float = 1.0
+var movement_force: float = 50.0
+
+# Reference to camera pivots
+var CamPivot_LR: Node3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Get reference to CamPivot_LR
+	CamPivot_LR = get_node("CamPivot_RotateLeftRight")
+
+# Called every physics frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+
+	# Get input direction
+	var input_direction = Vector3.ZERO
+
+	if Input.is_action_pressed("MoveForward"):
+		input_direction.z -= 1.0
+	if Input.is_action_pressed("MoveBackward"):
+		input_direction.z += 1.0
+	if Input.is_action_pressed("MoveLeft"):
+		input_direction.x += 1.0
+	if Input.is_action_pressed("MoveRight"):
+		input_direction.x -= 1.0
+
+	# If there's input, apply movement
+	if input_direction != Vector3.ZERO:
+		# Normalize the input direction
+		input_direction = input_direction.normalized()
+
+		# Get forward and right vectors from CamPivot_LR
+		var pivot_forward = -CamPivot_LR.global_transform.basis.z
+		var pivot_right = CamPivot_LR.global_transform.basis.x
+
+		# Flatten the forward vector to the ground plane (ignore Y)
+		pivot_forward.y = 0
+		pivot_forward = pivot_forward.normalized()
+
+		# Calculate movement direction relative to CamPivot_LR
+		var move_direction = (pivot_forward * input_direction.z) + (pivot_right * input_direction.x)
+		move_direction = move_direction.normalized()
+
+		# Apply central impulse for movement
+		# We use delta to make it framerate-independent
+		apply_central_impulse(move_direction * movement_force * delta)
